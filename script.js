996e9af119cdecd2f53ec5844b166ff9b09af14b
mohammad tavakoli <mohammadtavakolikia66@gmail.com>
2021-07-27 23:41:47 +0430

    next and prev button is now working

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
+const image=document.querySelector('img');
 const music=document.querySelector('audio');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
 
+//Music
+const songs=[
+    {
+        name: 'jacinto-1',
+        displayName: 'Electric Chill Machine',
+        artist: 'Jacinto Design'
+    },
+    {
+        name: 'jacinto-2',
+        displayName: 'Seven Nation Army (Remix)',
+        artist: 'Jacinto Design'
+    },
+    {
+        name: 'jacinto-3',
+        displayName: 'Goodnight, Disco Queen',
+        artist: 'Jacinto Design'
+    },
+    {
+        name: 'metric-1',
+        displayName: 'Im in The Front',
+        artist: 'Jacinto Design'
+    }
+]
+// Check if Playing
 let isPlaying = false;
 //Play
 function playSong(){
@@ -22,3 +49,38 @@ function pauseSong(){
 
 // Play or Pause Event List
 playBtn.addEventListener('click',() => (isPlaying ? pauseSong() : playSong()));
+
+// Update Dom
+function loadSong(song){
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`;
+}
+
+// Current Song
+let songIndex = 0;
+
+function nextSong(){
+    songIndex++;
+    if(songIndex > songs.length - 1){
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+    }
+function prevSong(){
+    songIndex--;
+    if(songIndex < 0){
+        songIndex = songs.length - 1;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+console.log(songIndex);
+
+
+
+// Event Listeners
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
